965a76dd1a689a094c2aae67239a06750447ed8a
Durga198T1A0428 <durgachimmili2001@gmail.com>
2023-08-29 10:53:32 +0530

    has_relationship

---
+++ person_is_doctor.py
@@ -0,0 +1,19 @@
+class person:
+    def __init__(self,name,age,gender):
+        self.name=name
+        self.age=age
+        self.gender=gender
+    def person_details(self):
+        print(self.name,"wants to become a doctor")
+class doctor(person):
+    def __init__(self,name,age,gender,doctor_specialization,doctor_location):
+        super().__init__(name,age,gender)
+        self.doctor_specialization=doctor_specialization
+        self.doctor_location=doctor_location
+    def doctor1(self):
+        print(self.doctor_specialization)
+        print(self.doctor_location)
+
+doctor_obj=doctor("asif",22,"male","heart","nrt")
+doctor_obj.doctor1()
+doctor_obj.person_details()
